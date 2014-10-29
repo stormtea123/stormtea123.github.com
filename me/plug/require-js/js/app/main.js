87910f27e171a6155f708d1ff18bdbbf86a29116
stormtea123 <stormte@gmail.com>
2014-10-29 11:33:37 +0800

    m

--- me/plug/require-js/js/app/main.js
+++
@@ -1,8 +0,0 @@
-define(["jquery", "jquery.alert", "jquery.event"], function($) {
-    //确定jquery.alert.js, jquery.event.js 加载完毕.
-    $(function() {
-        $("#handler-btn").click(function(){
-        	alert("我是jquery.alert.js, jquery.event.js 加载完毕，弹出的窗口！")
-        })
-    });
-});
